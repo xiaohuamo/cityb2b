ceeee469c7715324c103456b5f6e3e757a711d2a
harrymo <hhxx_2012@hotmail.com>
2022-01-26 22:13:25 +1100

    stage1 for all business mobile management panel update

--- core/web/ctl.company.php
+++ core/web/ctl.company.php
@@ -136,7 +136,7 @@ class ctl_company extends cmsPage
         $this->setData('商家首页', 'pagename');
         $this->setData('index', 'menu');
         $this->setData('商家中心 - ' . $this->site['pageTitle'], 'pageTitle');
-		//var_dump($this->current_business);exit;
+		var_dump($this->current_business['id'] . 'type id is'.$this->current_business['business_type_factory']);exit;
 		if($this->current_business['business_type_factory']==1) {
 
             $pclogin =trim(get2('pclogin')) ;
