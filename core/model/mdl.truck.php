9a3bccf6e8878f1e4d0147b2d71298c06b0fc2a6
harrymo <hhxx_2012@hotmail.com>
2022-02-14 13:10:16 +1100

    add driver name on packing summery list

--- core/model/mdl.truck.php
+++ core/model/mdl.truck.php
@@ -6,7 +6,9 @@ class mdl_truck extends mdl_base
 	protected $tableName = '#@_truck';
     public function getAllTruckOfBusiness($business_id) {
 		
-		$sql =" select * from cc_truck where business_id =$business_id and isAvaliable =1";
+		$sql =" select t.* ,concat(u.contactPersonFirstname,' ',u.contactPersonFirstname) as driverName  from cc_truck t
+         left join cc_user u on t.current_driver =u.id
+        where business_id =$business_id and isAvaliable =1";
 		$allTruckList  = $this->getListBySql($sql);
 		return $allTruckList;
 		
@@ -31,9 +33,10 @@ class mdl_truck extends mdl_base
 	
 	public function getAllOrdersTruckListwithCount($business_id,$delivery_date) {
 		
-		$sql =" select o.logistic_truck_No ,t.*,ifnull(count(*),0) as count  from cc_order o 
-				left join cc_truck t on o.logistic_truck_No=t.id
-				where o.logistic_delivery_date = ".strtotime($delivery_date)."  and o.business_userId=$business_id and o.coupon_status ='c01' and o.status=1 
+		$sql =" select o.logistic_truck_No ,t.*,ifnull(count(*),0) as count,concat(u.contactPersonFirstname,' ',u.contactPersonFirstname) as driverName from cc_order o 
+				left join cc_truck t on o.logistic_truck_No=t.id    
+                left join cc_user u on t.current_driver =u.id
+ 		    	where o.logistic_delivery_date = ".strtotime($delivery_date)."  and o.business_userId=$business_id and o.coupon_status ='c01' and o.status=1 
 				group by o.logistic_truck_No";
 		//var_dump($sql);exit;
 		$allTruckList  = $this->getListBySql($sql);
