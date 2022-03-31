656c1fba80c97e979fdf140dc7391f05896b4de7
harrymo <hhxx_2012@hotmail.com>
2022-03-31 17:18:41 +1100

    grade price and management menu update

--- core/b2b_2_0/b2b/lib/Credentials.php
+++ core/b2b_2_0/b2b/lib/Credentials.php
@@ -4,6 +4,7 @@ $credentials = [
     'password' => "2012Tang1",
     'client_id' => "3294bbf451f0c8aef74761efa74e31d8",
     'client_secret' => "3a160fa89347a99b7cd1e54575ec015d",
-    'endpoint_uri' => "https://api.whls.cityb2b.com"
+    'endpoint_uri' => "https://api.whls.cityb2b.com",
+    'business_id'=>'319188'
 ];
 ?>
