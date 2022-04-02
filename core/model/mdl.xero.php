bdadb0de41de8bc7546189323ff52a7a56ac280c
harrymo <hhxx_2012@hotmail.com>
2022-04-02 20:58:37 +1100

    grade price and management menu update

--- core/model/mdl.xero.php
+++ core/model/mdl.xero.php
@@ -221,14 +221,14 @@ class mdl_xero extends mdl_base
 
             $new_data['InvoiceID'] =$order_data['xero_id'];
             $new_data['InvoiceNumber'] =$order_data['invoice_id'];
-
-            $new_data['Date'] =date('m/d/Y',time());
+            $todaydays =0;
+            $new_data['Date'] =date('m/d/Y',strtotime("+$todaydays day"));
             $dueDays =$order_data['payment_period']+1;
 
             $new_data['DueDate'] =date('m/d/Y',strtotime("+$dueDays day"));
             $new_data['LineAmountTypes'] ="Exclusive";
 
-            $new_data['Reference'] ='';
+            $new_data['Reference'] =$order_data['orderId'];
             $new_data['BrandingThemeID'] ='';
             $new_data['CurrencyCode'] ='AUD';
             $new_data['Status'] ='DRAFT';
