706ed442513b2db4f38c436bd5f1da9dc833d7fe
harrymo <hhxx_2012@hotmail.com>
2022-04-27 18:06:07 +1000

    add unitqtyperbox

--- core/web/ctl.test.php
+++ core/web/ctl.test.php
@@ -3511,11 +3511,12 @@ public function xero_test_action() {
 		}
 		if(isset($_POST['btnUpdateContact'])) {
 			$contactList =$mdl_xero->getContactNameListForUpdateContactNameOnXero($this->current_business['id'],0,0,400);
-			var_dump($contactList);exit;
+		//	var_dump($contactList);exit;
 			//$response_arr =[];
 			foreach($contactList as $key=>$value){
 				$json_arr =json_encode($value);
 				$response_arr = $api->updateContact($credentials,$json_arr);
+				sleep(1);
 			}
 
 
