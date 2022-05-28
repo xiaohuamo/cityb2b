82869132e34c42e18f606eca8801394a1ddef721
harrymo <hhxx_2012@hotmail.com>
2022-05-28 18:56:08 +1000

    xero invocie auto pass update

--- core/web/ctl.company.php
+++ core/web/ctl.company.php
@@ -2622,7 +2622,7 @@ class ctl_company extends cmsPage
                  $this->auto_send_invoice_to_xero($id,$business_userid,'update');
                  $this->auto_send_invoice_to_xero($first_rec['id'],$business_userid,'update');
 
-                
+
 				echo json_encode(array('merge_to_another_order' => 1));
 			} else {
 				echo json_encode(array('merge_to_another_order' => 7));
